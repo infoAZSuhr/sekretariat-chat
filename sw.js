f45e2704a92f7f84f7d15b9d175a57b745cb5a64
infoAZSuhr <info@augenzentrum-suhr.ch>
2026-03-26 16:06:49 +0100

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,11 +1,15 @@
-const CACHE = 'sekretariat-v1';
+const CACHE = 'azs-intern-v2';
 
 self.addEventListener('install', e => {
   self.skipWaiting();
 });
 
 self.addEventListener('activate', e => {
-  e.waitUntil(clients.claim());
+  e.waitUntil(
+    caches.keys().then(keys =>
+      Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k)))
+    ).then(() => clients.claim())
+  );
 });
 
 self.addEventListener('fetch', e => {
@@ -13,4 +17,8 @@ self.addEventListener('fetch', e => {
   e.respondWith(
     fetch(e.request).catch(() => caches.match(e.request))
   );
+});
+
+self.addEventListener('message', e => {
+  if (e.data?.type === 'SKIP_WAITING') self.skipWaiting();
 });
